4e70476c319cdaadf0b35116ba11c064abfc2908
Aditi <aditi.verma.av19@gmail.com>
2024-05-06 21:14:47 -0700

    Added website components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './components/Navigation.tsx'
+import Navigation from './components/Navigation.tsx'
+import Footer from './components/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Navigation />
+    <Footer />
     <App />
   </React.StrictMode>,
 )
